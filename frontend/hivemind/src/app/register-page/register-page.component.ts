98a582c9fe6e288f426bb99fd45014cfa5bcd02a
hedge16 <vincenzo.riccio5@studenti.unina.it>
2025-03-19 17:13:39 +0100

    feat: enhance registration page with improved UI and validation; integrate Toastr for notifications; update navbar links

--- frontend/hivemind/src/app/register-page/register-page.component.ts
+++ frontend/hivemind/src/app/register-page/register-page.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
-import { RouterLink } from '@angular/router';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { RestBackendService } from '../_services/rest-backend/rest-backend.service';
 
 @Component({
   selector: 'app-register-page',
@@ -12,11 +14,36 @@ import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
-  registerForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(16)]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
   });
-
+  submitted = false;
+  toastr = inject(ToastrService);
+  restService = inject(RestBackendService);
+  router = inject(Router);
+  handleSignup() {
+    console.log("Signup form submitted");
+    this.submitted = true;
+    if(this.signupForm.invalid){
+      this.toastr.error("The data you provided is invalid!", "Oops! Invalid data!");
+    } else {
+      this.restService.signup({
+        firstName: this.signupForm.value.firstName as string,
+        lastName: this.signupForm.value.lastName as string,
+        email: this.signupForm.value.email as string,
+        password: this.signupForm.value.password as string,
+    }).subscribe({
+      error: (err) => {
+        this.toastr.error("The username you selected was already taken", "Oops! Could not create a new user");
+      },
+      complete: () => {
+        this.toastr.success(`You can now login with your new account`,`Congrats!`);
+        this.router.navigateByUrl("/login");
+        }
+      })
+    } 
+  }
 }
